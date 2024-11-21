6e9b7fe1bf29e3869bb4245fc71b425e510377f1
Christoph Herzog <chris@theduke.at>
2024-11-21 13:31:15 +0100

    Add phpmyadmin v5.2.1 code

---
+++ app/config.inc.php
@@ -0,0 +1,321 @@
+<?php
+/**
+ * phpMyAdmin sample configuration, you can use it as base for
+ * manual configuration. For easier setup you can use setup/
+ *
+ * All directives are explained in documentation in the doc/ folder
+ * or at <https://docs.phpmyadmin.net/>.
+ */
+
+declare(strict_types=1);
+
+/**
+ * This is needed for cookie based authentication to encrypt the cookie.
+ * Needs to be a 32-bytes long string of random bytes. See FAQ 2.10.
+ */
+$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+
+/**
+ * Servers configuration
+ */
+$i = 0;
+
+/**
+ * First server
+ */
+$i++;
+/* Authentication type */
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* Server parameters */
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+
+/**
+ * phpMyAdmin configuration storage settings.
+ */
+
+/* User used to manipulate with storage */
+// $cfg['Servers'][$i]['controlhost'] = '';
+// $cfg['Servers'][$i]['controlport'] = '';
+// $cfg['Servers'][$i]['controluser'] = 'pma';
+// $cfg['Servers'][$i]['controlpass'] = 'pmapass';
+
+/* Storage database and tables */
+// $cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
+// $cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
+// $cfg['Servers'][$i]['relation'] = 'pma__relation';
+// $cfg['Servers'][$i]['table_info'] = 'pma__table_info';
+// $cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
+// $cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
+// $cfg['Servers'][$i]['column_info'] = 'pma__column_info';
+// $cfg['Servers'][$i]['history'] = 'pma__history';
+// $cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
+// $cfg['Servers'][$i]['tracking'] = 'pma__tracking';
+// $cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
+// $cfg['Servers'][$i]['recent'] = 'pma__recent';
+// $cfg['Servers'][$i]['favorite'] = 'pma__favorite';
+// $cfg['Servers'][$i]['users'] = 'pma__users';
+// $cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
+// $cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
+// $cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
+// $cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
+// $cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
+// $cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
+
+/**
+ * End of servers configuration
+ */
+
+/**
+ * Directories for saving/loading files from server
+ */
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
+
+/**
+ * Whether to display icons or text or both icons and text in table row
+ * action segment. Value can be either of 'icons', 'text' or 'both'.
+ * default = 'both'
+ */
+//$cfg['RowActionType'] = 'icons';
+
+/**
+ * Defines whether a user should be displayed a "show all (records)"
+ * button in browse mode or not.
+ * default = false
+ */
+//$cfg['ShowAll'] = true;
+
+/**
+ * Number of rows displayed when browsing a result set. If the result
+ * set contains more rows, "Previous" and "Next".
+ * Possible values: 25, 50, 100, 250, 500
+ * default = 25
+ */
+//$cfg['MaxRows'] = 50;
+
+/**
+ * Disallow editing of binary fields
+ * valid values are:
+ *   false    allow editing
+ *   'blob'   allow editing except for BLOB fields
+ *   'noblob' disallow editing except for BLOB fields
+ *   'all'    disallow editing
+ * default = 'blob'
+ */
+//$cfg['ProtectBinary'] = false;
+
+/**
+ * Default language to use, if not browser-defined or user-defined
+ * (you find all languages in the locale folder)
+ * uncomment the desired line:
+ * default = 'en'
+ */
+//$cfg['DefaultLang'] = 'en';
+//$cfg['DefaultLang'] = 'de';
+
+/**
+ * How many columns should be used for table display of a database?
+ * (a value larger than 1 results in some information being hidden)
+ * default = 1
+ */
+//$cfg['PropertiesNumColumns'] = 2;
+
+/**
+ * Set to true if you want DB-based query history.If false, this utilizes
+ * JS-routines to display query history (lost by window close)
+ *
+ * This requires configuration storage enabled, see above.
+ * default = false
+ */
+//$cfg['QueryHistoryDB'] = true;
+
+/**
+ * When using DB-based query history, how many entries should be kept?
+ * default = 25
+ */
+//$cfg['QueryHistoryMax'] = 100;
+
+/**
+ * Whether or not to query the user before sending the error report to
+ * the phpMyAdmin team when a JavaScript error occurs
+ *
+ * Available options
+ * ('ask' | 'always' | 'never')
+ * default = 'ask'
+ */
+//$cfg['SendErrorReports'] = 'always';
+
+/**
+ * 'URLQueryEncryption' defines whether phpMyAdmin will encrypt sensitive data from the URL query string.
+ * 'URLQueryEncryptionSecretKey' is a 32 bytes long secret key used to encrypt/decrypt the URL query string.
+ */
+//$cfg['URLQueryEncryption'] = true;
+//$cfg['URLQueryEncryptionSecretKey'] = '';
+
+/**
+ * You can find more configuration options in the documentation
+ * in the doc/ folder or at <https://docs.phpmyadmin.net/>.
+ */
+
+
+/* Ensure we got the environment */
+$vars = [
+    'PMA_ARBITRARY',
+    'PMA_HOST',
+    'PMA_HOSTS',
+    'PMA_VERBOSE',
+    'PMA_VERBOSES',
+    'PMA_PORT',
+    'PMA_PORTS',
+    'PMA_SOCKET',
+    'PMA_SOCKETS',
+    'PMA_USER',
+    'PMA_PASSWORD',
+    'PMA_ABSOLUTE_URI',
+    'PMA_CONTROLHOST',
+    'PMA_CONTROLPORT',
+    'PMA_PMADB',
+    'PMA_CONTROLUSER',
+    'PMA_CONTROLPASS',
+    'PMA_QUERYHISTORYDB',
+    'PMA_QUERYHISTORYMAX',
+    'MAX_EXECUTION_TIME',
+    'MEMORY_LIMIT',
+    'PMA_UPLOADDIR',
+    'PMA_SAVEDIR',
+    'PMA_SSL',
+    'PMA_SSLS',
+];
+
+foreach ($vars as $var) {
+    $env = getenv($var);
+    if (!isset($_ENV[$var]) && $env !== false) {
+        $_ENV[$var] = $env;
+    }
+}
+if (isset($_ENV['PMA_QUERYHISTORYDB'])) {
+    $cfg['QueryHistoryDB'] = (bool) $_ENV['PMA_QUERYHISTORYDB'];
+}
+
+if (isset($_ENV['PMA_QUERYHISTORYMAX'])) {
+    $cfg['QueryHistoryMax'] = (int) $_ENV['PMA_QUERYHISTORYMAX'];
+}
+
+/* Arbitrary server connection */
+if (isset($_ENV['PMA_ARBITRARY']) && $_ENV['PMA_ARBITRARY'] === '1') {
+    $cfg['AllowArbitraryServer'] = true;
+}
+
+/* Play nice behind reverse proxys */
+if (isset($_ENV['PMA_ABSOLUTE_URI'])) {
+    $cfg['PmaAbsoluteUri'] = trim($_ENV['PMA_ABSOLUTE_URI']);
+}
+
+/* Figure out hosts */
+
+/* Fallback to default linked */
+$hosts = ['db'];
+
+/* Set by environment */
+if (! empty($_ENV['PMA_HOST'])) {
+    $hosts = [$_ENV['PMA_HOST']];
+    $verbose = [$_ENV['PMA_VERBOSE']];
+    $ports = [$_ENV['PMA_PORT']];
+    $ssls = [$_ENV['PMA_SSL']];
+} elseif (! empty($_ENV['PMA_HOSTS'])) {
+    $hosts = array_map('trim', explode(',', $_ENV['PMA_HOSTS']));
+    $verbose = array_map('trim', explode(',', $_ENV['PMA_VERBOSES']));
+    $ports = array_map('trim', explode(',', $_ENV['PMA_PORTS']));
+    $ssls = array_map('trim', explode(',', $_ENV['PMA_SSLS']));
+}
+
+if (! empty($_ENV['PMA_SOCKET'])) {
+    $sockets = [$_ENV['PMA_SOCKET']];
+} elseif (! empty($_ENV['PMA_SOCKETS'])) {
+    $sockets = explode(',', $_ENV['PMA_SOCKETS']);
+}
+
+/* Server settings */
+for ($i = 1; isset($hosts[$i - 1]); $i++) {
+    if (isset($ssls[$i - 1]) && $ssls[$i - 1] === '1') {
+        $cfg['Servers'][$i]['ssl'] = $ssls[$i - 1];
+    }
+    $cfg['Servers'][$i]['host'] = $hosts[$i - 1];
+    if (isset($verbose[$i - 1])) {
+        $cfg['Servers'][$i]['verbose'] = $verbose[$i - 1];
+    }
+    if (isset($ports[$i - 1])) {
+        $cfg['Servers'][$i]['port'] = $ports[$i - 1];
+    }
+    if (isset($_ENV['PMA_USER'])) {
+        $cfg['Servers'][$i]['auth_type'] = 'config';
+        $cfg['Servers'][$i]['user'] = $_ENV['PMA_USER'];
+        $cfg['Servers'][$i]['password'] = isset($_ENV['PMA_PASSWORD']) ? $_ENV['PMA_PASSWORD'] : '';
+    } else {
+        $cfg['Servers'][$i]['auth_type'] = isset($_ENV['PMA_AUTH_TYPE']) ? $_ENV['PMA_AUTH_TYPE']: 'cookie';
+    }
+    if (isset($_ENV['PMA_PMADB'])) {
+      $cfg['Servers'][$i]['pmadb'] = $_ENV['PMA_PMADB'];
+      $cfg['Servers'][$i]['relation'] = 'pma__relation';
+      $cfg['Servers'][$i]['table_info'] = 'pma__table_info';
+      $cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
+      $cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
+      $cfg['Servers'][$i]['column_info'] = 'pma__column_info';
+      $cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
+      $cfg['Servers'][$i]['history'] = 'pma__history';
+      $cfg['Servers'][$i]['recent'] = 'pma__recent';
+      $cfg['Servers'][$i]['favorite'] = 'pma__favorite';
+      $cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
+      $cfg['Servers'][$i]['tracking'] = 'pma__tracking';
+      $cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
+      $cfg['Servers'][$i]['users'] = 'pma__users';
+      $cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
+      $cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
+      $cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
+      $cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
+      $cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
+      $cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
+    }
+    if (isset($_ENV['PMA_CONTROLHOST'])) {
+      $cfg['Servers'][$i]['controlhost'] = $_ENV['PMA_CONTROLHOST'];
+    }
+    if (isset($_ENV['PMA_CONTROLPORT'])) {
+      $cfg['Servers'][$i]['controlport'] = $_ENV['PMA_CONTROLPORT'];
+    }
+    if (isset($_ENV['PMA_CONTROLUSER'])) {
+      $cfg['Servers'][$i]['controluser'] = $_ENV['PMA_CONTROLUSER'];
+    }
+    if (isset($_ENV['PMA_CONTROLPASS'])) {
+      $cfg['Servers'][$i]['controlpass'] = $_ENV['PMA_CONTROLPASS'];
+    }
+    $cfg['Servers'][$i]['compress'] = false;
+    $cfg['Servers'][$i]['AllowNoPassword'] = true;
+}
+for ($i = 1; isset($sockets[$i - 1]); $i++) {
+    $cfg['Servers'][$i]['socket'] = $sockets[$i - 1];
+    $cfg['Servers'][$i]['host'] = 'localhost';
+}
+/*
+ * Revert back to last configured server to make
+ * it easier in config.user.inc.php
+ */
+$i--;
+
+/* Uploads setup */
+if (isset($_ENV['PMA_UPLOADDIR'])) {
+    $cfg['UploadDir'] = $_ENV['PMA_UPLOADDIR'];
+}
+
+if (isset($_ENV['PMA_SAVEDIR'])) {
+    $cfg['SaveDir'] = $_ENV['PMA_SAVEDIR'];
+}
+
+if (isset($_ENV['MAX_EXECUTION_TIME'])) {
+    $cfg['ExecTimeLimit'] = $_ENV['MAX_EXECUTION_TIME'];
+}
+
+if (isset($_ENV['MEMORY_LIMIT'])) {
+    $cfg['MemoryLimit'] = $_ENV['MEMORY_LIMIT'];
+}
